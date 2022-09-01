36ac3dd5194b5df79d6cc27300da70d99c9304cc
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-09-01 12:28:42 +0530

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const PORT = 5500;
 const connect = require("./configs/db");
+const PORT = process.env.PORT || 5500;
 
 const productController = require("./controllers/products.controller");
 const reviewController = require("./controllers/review.controller");
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/product", productController);
 app.use("/review", reviewController);
 
-app.listen(PORT, async function () {
+app.listen(PORT, async () => {
   try {
     await connect();
   } catch (e) {
